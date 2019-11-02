ddda2d0767efa21e47e04830ad22b387d24d875e
SanjaLakic1 <49113791+SanjaLakic1@users.noreply.github.com>
2019-11-02 14:23:52 +0100

    Add files via upload

--- server.php
+++ server.php
@@ -1,12 +1,34 @@
-<?php
-$con=mysqli_connect("localhost","my_user","my_password","recepti");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+<html>
+<body>
 
-// Perform queries
-mysqli_query($con,"SELECT * FROM Persons");
-mysqli_close($con);
+<?php
+ include("config.php");
+
+if(!empty($_POST['check_list'])){
+	$selektovaneNamirnice = "(";
+	foreach($_POST['check_list'] as $selected){
+		$selektovaneNamirnice .= $selected;
+		$selektovaneNamirnice .= ",";
+	}
+	$selektovaneNamirnice = rtrim($selektovaneNamirnice,',');
+	$selektovaneNamirnice .=")";
+	//echo $selektovaneNamirnice;
+	$query = "select distinct r.Ime as htmlName,re.Ime as nameRecepta, count(*) as broj from receptnamirnica r
+	join recept re on r.ReceptID = re.Id where r.NamirniceId in ";
+	$query .= $selektovaneNamirnice;
+	$query .= " group by r.receptID having broj= ( select count(*) from receptnamirnica where receptId = r.ReceptID)";
+	
+	$result = mysqli_query($db,$query);
+	if(mysqli_num_rows($result)==0) echo("Za unete namirnice nisu pronadjeni recepti");
+	while($row = mysqli_fetch_array($result)){
+		echo "<a href='recepti/".$row['htmlName']."'><h3>".$row['nameRecepta']."</h3></a>";
+	}
+		 
+}
+$db->close();
+
+
 ?>
+ 
+</body>
+</html>
